4067339ab2e857efaaba8c0d6bf69fc23781ef83
vadimdubka <vadimdubka@gmail.com>
2019-02-05 10:00:31 +0300

    initial commit

---
+++ src/main/java/com/vdibka/runtracker/RuntrackerApplication.java
@@ -0,0 +1,14 @@
+package com.vdibka.runtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntrackerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/vdibka/runtracker/RuntrackerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.vdibka.runtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RuntrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
